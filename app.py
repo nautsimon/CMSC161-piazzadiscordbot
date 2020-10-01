cbe1bdfd66f1eb037df6a8a26fa0aae072d9703e
Simon mahns <ak.simonm@gmail.com>
2020-09-30 20:37:16 -0400

    updating to most recent version

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from flask import Flask, jsonify, request 
-from flask_cors import CORS 
-import pizza #pizza is a config file for the piazza api wrapper. DM if you want access.
-import db #db is a config file that inits mongodb. DM if you want access.
-#import dis #dis is a config file that holds the api keys for discord. DM if you want access.
+from flask import Flask
+import pizzapizzasecret #pizzapizzasecret has all the secrets
 from piazza_api import Piazza
 from piazza_api import network as net
 import re
+import pymongo
+import time
 #shhhhhhhhh
 import requests
 app = Flask(__name__)
@@ -15,68 +14,73 @@ def sendPayload(postObj):
   # print("https://piazza.com/class/keke5ooeun21ot?cid="  + str(postObj['nr']))
   # print(re.sub('<[^<]+?>', '', str(postObj['history'][0]['content'])) + "...")
   # print(postObj['unique_views'])
-  data = {}
-  data["content"] = "New post: " + postObj['created']
-  data["username"] = "CMSC PIAZZA BOT"
-  data["embeds"] = []
-  embed = {}
-  embed["description"] = re.sub('<[^<]+?>', '', str(postObj['history'][0]['content']))
-  embed["title"] = postObj['history'][0]['subject']
-  embed["url"] = "https://piazza.com/class/keke5ooeun21ot?cid="  + str(postObj['nr'])
-  embed["color"] = 0XA5E0FE
-  data["embeds"].append(embed)
-  result = requests.post(pizza.url, json=data, headers={"Content-Type": "application/json"})
+    data = {}
+    data["content"] = "New post: " + postObj['created']
+    data["username"] = "CMSC PIAZZA BOT"
+    data["embeds"] = []
+    embed = {}
+    embed["description"] = re.sub('<[^<]+?>', '', str(postObj['history'][0]['content']))
+    embed["title"] = postObj['history'][0]['subject']
+    embed["url"] = "https://piazza.com/class/keke5ooeun21ot?cid="  + str(postObj['nr'])
+    embed["color"] = 0XA5E0FE
+    data["embeds"].append(embed)
+    result = requests.post(pizzapizzasecret.url, json=data, headers={"Content-Type": "application/json"})
 
-  try:
-      result.raise_for_status()
-  except requests.exceptions.HTTPError as err:
-      print(err)
-  else:
-      print("succ {}.".format(result.status_code))
+    try:
+        result.raise_for_status()
+        time.sleep(5)
+    except requests.exceptions.HTTPError as err:
+        print(err)
+    else:
+        print("succ {}.".format(result.status_code))
 
 
-@app.route("/test")
+@app.route("/check")
 def test():
-  existingPosts = []
-  query = db.table.find() 
-  output = {} 
-  i = 0
-  for x in query: 
-    output[i] = x 
-    output[i].pop('_id') 
-    existingPosts.append(output[i]['ID'] ) 
-    i += 1
-  #print(existingPosts)
-  p = Piazza()
-  p.user_login(pizza.email, pizza.password)
-  ds = p.network(pizza.net)
-  #print("ds", ds.iter_all_posts())
-  posts = ds.iter_all_posts()
-  for post in posts:
-    print("post", post)
-    if post['history'][0]['subject']  in existingPosts:
-      sendPayload(post)
-      queryObject = { 
-        'ID': post['history'][0]['subject'],
-      }
-      #query = db.table.insert_one(queryObject) 
+    client = pymongo.MongoClient(pizzapizzasecret.dbsecret)
+    db = client.get_database('piazza-posts')
+    table = db.posts
+    #print("table", table)
+    existingPosts = []
+    query = table.find()
+    output = {}
+    i = 0
+    for x in query:
+        output[i] = x
+        output[i].pop('_id')
+        existingPosts.append(output[i]['ID'] )
+        i += 1
+    #print(existingPosts)
+    p = Piazza()
+    p.user_login(pizzapizzasecret.email, pizzapizzasecret.password)
+    ds = p.network(pizzapizzasecret.net)
+    #print("ds", ds.iter_all_posts())
+    posts = ds.iter_all_posts()
+    for post in posts:
+        #print("post", post)
+        if post['history'][0]['subject'] not in existingPosts:
+            sendPayload(post)
+            queryObject = {
+                'ID': post['history'][0]['subject'],
+            }
+            query = db.table.insert_one(queryObject)
   # ds_posts = ds.get_filtered_feed(net.UnreadFilter())
   # if len(ds_posts['feed']) > 0:
   #   print("length", len(ds_posts['feed']))
   #   for x in range(0, len(ds_posts['feed'])):
   #     print(ds_posts['feed'][x]['subject'])
 
-  return "Connected to the data base!"
+    return "success"
 
 
 @app.route('/')
 def rootAcc():
-    return 'h̸̨̨̧̳̯̻͕̩̙̟̯̐́̒͂̂͝ē̸̢̡̧̡̡̡̧̛̛̠̲̥̫̘͈̦̭̺͚̠̹̲̟̭͈͎̫̱̤͙̘̙̘͎̼̱͓͍͈̯̻̞̹͍̻̦͖̤̪̞̗̠͕̖͎̠̙̖̲͉͓͕̖͓̭̦̙̝̟̣̖̭͈͚̱̯̜̲̥͈̯̙̻͒̀̏̄̋͌͊̇̋̈́͑͆̌̈́̋̽̃̍͌́͑̓̋̀̊̈́̒̃̇̾̒͆̈́̓̋̇̇͂͆̒̏̿̓̑̀̽̂͐̌̕̕̚͘̕ͅl̸̢̡̛̩͇̦̠͇̤͖̬̝͙̰̟̱̺̯͓̪̤͖̩̞̩͚̠̫͓̠̥̎̾̔̐̌͗̋̀͛̊̆̓̀̽͊̈́͗͌̄͋̀́̈͌͐̆̈͛͑̽̃̊̆̓̅̍̃́́̿́̾͂̿́̀̀͆̾̓̏͛͘͘̕͝͠͝͝͠l̴̨̨̨̨̧̡̢̢̢̨̧̛̗̥̙̮̫͎̼̮̤͕̠̹̙͇̜͕̰̰̲͇̤̗̙̜͔̻͇̖̦͉̻̪̝̬̭̯̲̻̹̱̤̼̪̲̻̩͇͚͕̭̼͊̌̃͂͑́̚͜͜͜ͅͅͅơ̶̧̢̨̧̡̘͓̤̼̫͎̣̞͕̯͇͕̹̬͉̝͚̮͖͍̖̖̪̣͙̱̰̾̽̏͛̐̒͆̄̿̆͒͊̋̃̆̏̆̈́̈́̈̀̏̈́̾͌͑̉͐̍͋̅̈́̐͐̈́͗͗̓̀̈͊͋̈́̎̄͗̕̚͝͝͝͝͝͝͝͠'
+    return 'yes, cmsc is misspelled h̸̨̨̧̳̯̻͕̩̙̟̯̐́̒͂̂͝ē̸̢̡̧̡̡̡̧̛̛̠̲̥̫̘͈̦̭̺͚̠̹̲̟̭͈͎̫̱̤͙̘̙̘͎̼̱͓͍͈̯̻̞̹͍̻̦͖̤̪̞̗̠͕̖͎̠̙̖̲͉͓͕̖͓̭̦̙̝̟̣̖̭͈͚̱̯̜̲̥͈̯̙̻͒̀̏̄̋͌͊̇̋̈́͑͆̌̈́̋̽̃̍͌́͑̓̋̀̊̈́̒̃̇̾̒͆̈́̓̋̇̇͂͆̒̏̿̓̑̀̽̂͐̌̕̕̚͘̕ͅl̸̢̡̛̩͇̦̠͇̤͖̬̝͙̰̟̱̺̯͓̪̤͖̩̞̩͚̠̫͓̠̥̎̾̔̐̌͗̋̀͛̊̆̓̀̽͊̈́͗͌̄͋̀́̈͌͐̆̈͛͑̽̃̊̆̓̅̍̃́́̿́̾͂̿́̀̀͆̾̓̏͛͘͘̕͝͠͝͝͠l̴̨̨̨̨̧̡̢̢̢̨̧̛̗̥̙̮̫͎̼̮̤͕̠̹̙͇̜͕̰̰̲͇̤̗̙̜͔̻͇̖̦͉̻̪̝̬̭̯̲̻̹̱̤̼̪̲̻̩͇͚͕̭̼͊̌̃͂͑́̚͜͜͜ͅͅͅơ̶̧̢̨̧̡̘͓̤̼̫͎̣̞͕̯͇͕̹̬͉̝͚̮͖͍̖̖̪̣͙̱̰̾̽̏͛̐̒͆̄̿̆͒͊̋̃̆̏̆̈́̈́̈̀̏̈́̾͌͑̉͐̍͋̅̈́̐͐̈́͗͗̓̀̈͊͋̈́̎̄͗̕̚͝͝͝͝͝͝͝͠'
 
 
 
 if __name__ == '__main__':
-    app.run(port=8000, debug=True)
+    app.run(host='0.0.0.0', port=8081)
 
 
 
